1f4ac9da93e9658ebac82add64cea1a2e13ccc01
Vitali <Kasvital@gmail.com>
2017-02-03 23:08:07 +0300

    MetaMap update ruRU

--- MetaMapBWP/localisation.lua
+++ MetaMapBWP/localisation.lua
@@ -26,6 +26,37 @@ BWP_INFO_TEXT1 = "You Must Be in The Same Zone\nto Set "
 BWP_INFO_TEXT3 = " as  Waypoint"
 BWP_INFO_TEXT2 = "Set "
 
+if(GetLocale() == "ruRU")  then
+
+BWP_NILLOCATION = "Нет доступных направлений"
+BWP_SELECTMSG = "Выбрать направление"
+BINDING_NAME_BWP_CLEAR = "Очистить Маршрутную точку";
+BWP_CLEAR_DEST = "Очистить направление"
+BWP_CLEAR_ARRIVE = "Очистить Маршрутную точку при прибытии"
+BWP_NPC_TEXT = "ЗNPC: "
+BWP_ARRIVED = "Прибытие!"
+BWP_BUTTON_TOOLTIP = "MetaMapBWP\nЛевоеНажатие для открытия меню\ПравоеНажатие для перемещения"
+BWPGROUPERROR = "Вы должны быть членом Группы или Рейда и в той же зоне что и цель для лучшей маршрутной точке"
+BWPGROUPERROR1 = "Запрошенная цель вне текущего рейда."
+BWPGROUPERROR2 = "Запрошенная цель вне текущей группы."
+BWP_QUEST_NPCSTRING ="NPC для Заданий"
+
+BWP_ALWAYS_LOAD = "Всегда загружаться при запуске"
+BWP_SHOW_MAPPOINTS = "Показывать точки в меню"
+BWP_SHOW_CORPSE = "Показывать Маршрутную точку для трупа"
+BWP_SHOW_YARDS = "Показывать расстояние в ярдах"
+BWP_SHOW_METRES = "Показывать расстояние в метрах"
+BWP_CLEARDIST_TEXT = "Сообщение о расстоянии от отправления до прибытия"
+BWP_SHOW_QNPC = "Показывать NPC для заданий"
+
+BWP_CORPSE_TEXT = "Труп"
+BWP_OPTION_TEXT = "Настройки"
+BWP_INFO_TEXT1 = "Вы должны быть в той же зоне\nчтобы установить "
+BWP_INFO_TEXT3 = " как Маршрутная точка"
+BWP_INFO_TEXT2 = "Установить "
+
+end
+
 if(GetLocale() == "deDE")  then
 
 BWP_NILLOCATION = "Keine Ziele verf\195\188gbar"
