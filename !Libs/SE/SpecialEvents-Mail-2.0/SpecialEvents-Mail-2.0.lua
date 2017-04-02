eb59d9f45065980c59218adb575a1e880d82e83a
Vitali <Kasvital@gmail.com>
2017-04-02 20:00:26 +0300

    Libs
    fix SE:Mail lib for ruRU client

--- !Libs/SE/SpecialEvents-Mail-2.0/SpecialEvents-Mail-2.0.lua
+++ !Libs/SE/SpecialEvents-Mail-2.0/SpecialEvents-Mail-2.0.lua
@@ -1,14 +1,15 @@
 --[[
 	Name: SpecialEvents-Mail-2.0
-	Revision: $Rev: 9518 $
+	Revision: $Rev: 9519 $
 	Author: Tekkub Stoutwrithe (tekkub@gmail.com)
 	Website: http://wiki.wowace.com/index.php/SpecialEvents_Addon
 	Documentation: http://wiki.wowace.com/index.php/SpecialEvents-Mail-2.0
 	SVN: svn://svn.wowace.com/root/trunk/SpecialEventsEmbed/SpecialEvents-Mail-2.0
 	Description: Special events for mail (received, auction notices, etc)
 	Dependencies: AceLibrary, AceEvent-2.0
+	-9519
+	--fix for ruRU 82-105 lines
 ]]
-
 
 local vmajor, vminor = "SpecialEvents-Mail-2.0", "$Revision: 9518 $"
 
@@ -20,7 +21,6 @@ local lib = {}
 AceLibrary("AceEvent-2.0"):embed(lib)
 
 local closedelay = 5
-
 
 -- Activate a new instance of this library
 function activate(self, oldLib, oldDeactivate)
@@ -35,7 +35,6 @@ else self.vars = {} end
 	if oldDeactivate then oldDeactivate(oldLib) end
 end
 
-
 --------------------------------
 --      Tracking methods      --
 --------------------------------
@@ -44,11 +43,9 @@ function lib:PLAYER_ENTERING_WORLD()
 	self.vars.zoneevent = true
 end
 
-
 function lib:MAIL_CLOSED()
 	self.vars.lastclose = GetTime()
 end
-
 
 function lib:UPDATE_PENDING_MAIL()
 	if self.vars.lastclose and (self.vars.lastclose + closedelay) > GetTime() then self.vars.ignorenext = true end
@@ -67,7 +64,6 @@ function lib:UPDATE_PENDING_MAIL()
 	self:TriggerEvent("SpecialEvents_MailReceived")
 end
 
-
 -- Events that don't fire UPDATE_PENDING_MAIL like they should
 local brokenevents = {
 	[ERR_AUCTION_WON_S]     = false,
@@ -83,15 +79,30 @@ local eventnames = {
 	[ERR_AUCTION_EXPIRED_S] = "EXPIRED",
 	[ERR_AUCTION_REMOVED_S] = "REMOVED",
 }
+local ru=GetLocale()=="ruRU" --by CFM
 local aucstr = {}
-for i in pairs(brokenevents) do aucstr[i] = string.gsub(i, "%%[^%s]+", "(.+)") end
+for i in pairs(brokenevents) do
+	if ru and i==ERR_AUCTION_EXPIRED_S then  --by CFM
+		aucstr[i] = string.gsub(i,"%%[^%s]+",".+)")  --by CFM
+	elseif ru and (i==ERR_AUCTION_SOLD_S or i==ERR_AUCTION_REMOVED_S) then  --by CFM
+		aucstr[i] = string.gsub(i,"\"%%[^%s]+","\"(.+)\"")  --by CFM
+	elseif ru and i==ERR_AUCTION_WON_S then  --by CFM
+		aucstr[i] = string.gsub(i,"%%[^%s]","(.+)")  --by CFM
+	else  --by CFM
+		aucstr[i] = string.gsub(i,"%%[^%s]+","(.+)")
+	end  --by CFM
+	
+end
 
 function lib:CHAT_MSG_SYSTEM(msg)
 	if not msg then return end
-	
 	for i,searchstr in pairs(aucstr) do
+		if not searchstr then return end --by CFM
 		local _, _, item = string.find(msg, searchstr)
 		if item then
+			if ru and string.find(item,"%(") then  --by CFM
+				item = string.sub(item,2,string.len(item)-1)  --by CFM
+			end  --by CFM
 			self:TriggerEvent("SpecialEvents_AHAlert", eventnames[i], item)
 			if brokenevents[i] then
 				self:TriggerEvent("SpecialEvents_MailReceived")
@@ -100,7 +111,6 @@ function lib:CHAT_MSG_SYSTEM(msg)
 		end
 	end
 end
-
 
 --------------------------------
 --      Load this bitch!      --
