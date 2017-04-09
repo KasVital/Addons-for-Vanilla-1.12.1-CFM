6e08b63b5d5ae82f141d5b8919b9df6ac438b21e
Vitali <Kasvital@gmail.com>
2017-04-09 20:10:23 +0300

    Quesie-QuestieRU
    little fix for quest

--- !Questie/Database/addendum.lua
+++ !Questie/Database/addendum.lua
@@ -28335,7 +28335,8 @@ QuestieHashMap = {
   ['finishedBy']="Zureetha Fargaze",
   ['level']=2,
   ['questLevel']='5',
-  ['rr']=178
+  ['rr']=178,
+  ['rq']=611883498
  },
  [4210602997]={
   ['name']="To Ironforge for Yagyin's Digest",
