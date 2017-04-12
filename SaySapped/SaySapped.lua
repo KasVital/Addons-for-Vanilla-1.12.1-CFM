76f4c2438acc598b225a9e56b1d8e1b2a5c2dd3e
Vitali <Kasvital@gmail.com>
2017-04-12 20:22:26 +0300

    SaySapped
    big rework and translate

--- SaySapped/SaySapped.lua
+++ SaySapped/SaySapped.lua
@@ -1,18 +1,64 @@
---russian by CFM
-local SaySapped = CreateFrame("Frame")
-SaySapped.playername = UnitName("player")
+--[[
+	SaySapped: Says "Sapped!" when you get sapped allowing you to notify nearby players about it.
+	Also works for many other CCs.
+	Author: Coax  - Nostalrius PvP
+	Translate and rework by: CFM - Elysium
+	Original idea: Bitbyte of Icecrown
+--]]
 
-SaySapped:SetScript("OnEvent", function(_,_,_,event, _,_,sourceName, _,_,_,destName, _,_,spellId)
-if ((spellId == 6770)
-	and (destName == SaySapped.playername)
-	and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH"))
-then
-	SendChatMessage("Sapped","SAY")
-	if (GetLocale()=="ruRU") then
-		DEFAULT_CHAT_FRAME:AddMessage("Ошеломление от: "..(sourceName or "(unknown)"))
+-- Slash Command
+SLASH_SAYSAPPED1 = '/saysapped'
+SLASH_SAYSAPPED2 = '/ssap'
+function SlashCmdList.SAYSAPPED(msg, editbox)
+  	if SaySappedConfig then
+		SaySappedConfig = false
+		DEFAULT_CHAT_FRAME:AddMessage(SS_DISABLED)
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("Sapped by: "..(sourceName or "(unknown)"))
+		SaySappedConfig = true
+		DEFAULT_CHAT_FRAME:AddMessage(SS_ENABLED)
 	end
 end
+
+-- Translated by CFM
+if GetLocale()=="ruRU" then
+	SS_Sapped='Sapped!'
+	SS_SpellSap='"Ошеломление".'
+	SS_Loaded='|cffffff55SaySapped загружен /saysapped. От Coax, доработка и перевод CFM.'
+	SS_SELFHARMFULL='Вы находитесь'
+	SS_DISABLED='|cffffff55SaySapped выключен!'
+	SS_ENABLED='|cffffff55SaySapped включен!'
+else
+	SS_Sapped='Sapped!'
+	SS_SpellSap=' Sap.'
+	SS_Loaded='|cffffff55SaySapped loaded, type /saysapped to toggle on and off. Made by Coax.'
+	SS_SELFHARMFULL='You are'
+	SS_DISABLED='|cffffff55SaySapped disabled!'
+	SS_ENABLED='|cffffff55SaySapped enabled!'
+end
+
+local SaySapped = CreateFrame("Frame",nil,UIParent)
+SaySapped:RegisterEvent("ADDON_LOADED")
+
+SaySapped:SetScript("OnEvent", function()
+	if arg1 == "SaySapped" then
+		DEFAULT_CHAT_FRAME:AddMessage(SS_Loaded)
+		if not SaySappedConfig then
+			SaySappedConfig = true;
+		end
+		SaySapped.checkbuff = CreateFrame("Frame")
+		SaySapped.checkbuff:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE")
+		SaySapped.checkbuff:SetScript("OnEvent", function()
+			if string.find(arg1, SS_SELFHARMFULL) then
+				SaySapped_FilterDebuffs(arg1)
+			end
+		end)
+	end
 end)
-SaySapped:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+-- Check if sapped
+function SaySapped_FilterDebuffs(spell)
+	if string.find(spell, SS_SpellSap) and SaySappedConfig then
+		SendChatMessage(SS_Sapped,"SAY")
+		DEFAULT_CHAT_FRAME:AddMessage(SS_Sapped)
+	end
+end
